ef27844897250da81c94e26f3274e57bcaeaa418
Eric Dodgion <Barry.manowar@gmail.com>
2021-04-25 19:08:22 -0600

    this is hard

--- Develope/index.js
+++ Develope/index.js
@@ -1,35 +1,153 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require("util");
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const generateHtml = require('./src/template')
 //const Employee = require('/Employee');
+const teamMembers = [];
 
 
 const promptUser = () => {
-    return inquirer.prompt([
-      {
-      type: 'input',
-      name: 'name',
-      message: "Waht is your name?"
-      },
-      { type: 'input',
-      name: 'email',
-      message:"What is your email address?"
-    },
+    
+  const generateTeam = () => {
+  inquirer.prompt([
     {
       type: 'list',
       name: 'role',
-      message:"What is your role?",
+      message:"What is this Emplyee's role?",
       choices: 
       ['Manager',
       'Engineer', 
       'Intern'],
     }
 ])
+.then(answers => {
+  if (answers.employeeChoice === 'Manager') {
+      generateManager();
+  } else if (answers.employeeChoice === 'Engineer'){
+      generateEngineer();
+  } else if (answers.employeeChoice === 'Intern') {
+    generateIntern();
+}else {
+      return generateTeam();
+  }
+})
+}
+
+const generateManager = () => {
+  inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Waht is the Manager's name?"
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "What is the Manager's ID number?"
+      },
+        { type: 'input',
+        name: 'email',
+        message:"What is the Manager's email address?"
+      },
+      {
+        type: 'input',
+        name: 'managerOfficeNum',
+        message: "What is the Manager's Office Number?"
+    }
+  ])
+  .then(answers => {
+    const manager = new Manager(
+        answers.managerName,
+        answers.managerId,
+        answers.managerEmail,
+        answers.managerOfficeNum,
+    )
+    teamMembers.push(manager); 
+    pickEmployee();
+  })
+
 };
+
+
+const generateEngineer = () => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'engineerName',
+          message:"What is the Engineer's name?"
+      },
+      {
+          type: 'input',
+          name: 'engineerId',
+          message: "What is the Engineer's ID Number?"
+      },
+      {
+          type: 'input',
+          name: 'engineerEmail',
+          message: "What is the Engineers Email?"
+      },
+      {
+          type: 'input',
+          name: 'engineerGithub',
+          message: "What is the Engineers Github account?"
+      }
+   ])
+   .then(answers => {
+    const engineer = new Engineer(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.engineerGithub,
+    )
+    teamMembers.push(engineer);
+    pickEmployee();
+})
+};
+
+
+    const generateIntern = () => {
+      inquirer.prompt([
+          {
+              type: 'input',
+              name: 'internName',
+              message: "What is the Interns name?"
+          },
+          {
+              type: 'input',
+              name: 'internId',
+              message: "What is the Interns ID Number?"
+          },
+          {
+              type: 'input',
+              name: 'internEmail',
+              message: "What is the Interns Email?"
+          },
+          {
+              type: 'input',
+              name: 'internSchool',
+              message: "What School did the Intern go to?"
+          }
+        ])
+        .then(answers => {
+          const intern = new Intern(
+              answers.internName,
+              answers.internId,
+              answers.internEmail,
+              answers.internSchool,
+          )
+          teamMembers.push(intern);
+          pickEmployee();
+      })
+      }
+    };
+
 
 const init = () => {
   promptUser()
-    .then((answers) => writeFileAsync('gernerateIndex.html', generateReadMe(answers)))
+    .then((answers) => writeFileAsync('gernerateIndex.html', generateTeam(answers)))
     .then(() => console.log('Successfully wrote to util'))
     .catch((err) => console.error(err));
 };
