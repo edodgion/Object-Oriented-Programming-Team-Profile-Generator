7c4557497e70de0dfbdcf24f27f8455878dbb377
Eric Dodgion <Barry.manowar@gmail.com>
2021-05-07 21:56:13 -0600

    Employee test

--- Develope/__tests__/Employee.test.js
+++ Develope/__tests__/Employee.test.js
@@ -1,27 +1,44 @@
-const Letter = require("../lib/Epmloyee");
-
-describe("Employee class", () => {
-  it("", () => {
-    expect(new Employee().name()).toBe("");
-  });
-
-  it("getName returns name", () => {
-    expect(getName()).toBe("");
-  });
-
-  describe("guess", () => {
-    it("Correct guess returns true", () => {
-      expect(new Letter("j").guess("j")).toBe(true);
-    });
-
-    it("Incorrect guess returns false", () => {
-      expect(new Letter("j").guess("l")).toBe(false);
-    });
-  });
-
-  describe("getRole", () => {
-    it("returns character", () => {
-      expect(new Letter("l").getSolution()).toBe("l");
-    });
-  });
+const Employee = require("../lib/Employee");
+test('create an employee', () => {
+    const e = new Employee()
+    expect(typeof(e)).toBe('object')
 });
+test('Can set name through constructor', () => {
+    const name = "Eric" 
+    const e = new Employee(name)
+    expect(e.name).toBe(name)
+});
+test('Can set ie through constructor', () => {
+    const id = 100;
+    const e = new Employee("Eric", id)
+    expect(e.id).toBe(id)
+});
+test('Can set emaie through constructor', () => {
+    const email = "Eric@email.com";
+    const e = new Employee("Eric", 1, email)
+    expect(e.email).toBe(email)
+});
+test('Can we get name through getName()', () => {
+    const name = "Eric";
+    const e = new Employee(name)
+    expect(e.getName()).toBe(name)
+});
+test('Can we get Ie through getID()', () => {
+    const id = 1;
+    const e = new Employee('Eric', id)
+    expect(e.getId()).toBe(id)
+});
+test('Can we get Emaie through getEmail()', () => {
+    const email = 'Eric@email.com'
+    const e = new Employee('Eric', 1, email)
+    expect(e.getEmail()).toBe(email)
+});
+test('Can we get Role through getRole()', () => {
+    const role = 'Employee'
+    const e = new Employee('Eric', 1, 'Eric@email.com',)
+    expect(e.getRole()).toBe(role)
+});
+
+
+
+
